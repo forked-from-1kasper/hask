b64e4b131af691d2530787e5bb68e9e494cc291e
Siegmentation <siegmentationfault@yandex.ru>
2019-12-04 16:41:43 +0700

    lost

---
+++ hask/lang/type_vars.py
@@ -0,0 +1,15 @@
+from string import ascii_lowercase
+from .syntax import H, t
+
+class TypeVarSyntax(str):
+    def __rshift__(dom, cod):
+        return H/ dom >> cod
+
+    def __call__(self, *args):
+        return t(self, *args)
+
+reserved = "mpdt"
+
+for alpha in ascii_lowercase:
+    if alpha not in reserved:
+        globals()[alpha] = TypeVarSyntax(alpha)
